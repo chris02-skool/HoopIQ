d6929728f4f3c49032a038fa05dcea40fe3abb2b
Christopher Hong <christopherhong02@yahoo.com>
2025-11-05 17:53:15 -0800

    added 2 buttons to show all the makes or all the misses

--- shot_selection.py
+++ shot_selection.py
@@ -4,16 +4,25 @@ import streamlit as st
 
 def selected_shots_idx(shots):
     shot_labels = [f"Shot {i+1} ({s['result']})" for i, s in enumerate(shots)]
+    makes = [shot_labels[i] for i, s in enumerate(shots) if s['result'] == "Make"]
+    misses = [shot_labels[i] for i, s in enumerate(shots) if s['result'] == "Miss"]
+
     if 'selected_shots' not in st.session_state:
         st.session_state.selected_shots = shot_labels.copy()
 
-    col1, col2 = st.columns([1,1])
+    col1, col2, col3, col4 = st.columns([1,1,1,1])
     with col1:
         if st.button("Select All Shots"):
             st.session_state.selected_shots = shot_labels.copy()
     with col2:
         if st.button("Clear All Shots"):
             st.session_state.selected_shots = []
+    with col3:
+        if st.button("Show All Makes"):
+            st.session_state.selected_shots = makes
+    with col4:
+        if st.button("Show All Misses"):
+            st.session_state.selected_shots = misses
 
     st.session_state.selected_shots = st.multiselect(
         "Choose which shots to display:",
